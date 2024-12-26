1083e484ebb8834f8de7b0e0279e85f9fdd4c2b5
arthur albuquerque <arthuralbuquerque.hsh@gmail.com>
2024-12-26 15:43:17 -0300

    MyRazorApp
    Projeto Para Entender melhor o Asp.Net e  a Arquitetura Razor Pages

---
+++ Pages/index.cshtml.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyRazorApp.Pages
+{
+    public class Index : PageModel
+    {
+        public void OnGet()
+        {
+        }
+        public void OnPost()
+        {
+
+        }
+    }
+}

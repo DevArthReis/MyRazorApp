d310f18803c86ba408710d3382f69c3f102f9ef6
arthur albuquerque <arthuralbuquerque.hsh@gmail.com>
2024-12-27 16:21:15 -0300

    Att

--- Pages/index.cshtml.cs
+++ Pages/index.cshtml.cs
@@ -5,12 +5,30 @@ namespace MyRazorApp.Pages
 {
     public class Index : PageModel
     {
-        public void OnGet()
+        public List<Category>Categories{ get; set; }= new ();
+        public async Task OnGet()
         {
+            
+            await Task.Delay(5000);
+            for (var i = 0;i <= 100;i++)
+            {
+                Categories.Add(
+                    new Category (
+                    i,//id
+                    $"Categoria{i}",//Title
+                    i*18.95M));//Price
+            }
         }
         public void OnPost()
         {
 
         }
+    }
+
+    public record Category(int id,
+    string title,
+    decimal price)
+    {
+        
     }
 }

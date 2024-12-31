d81eb049ad21975c0e34e631d85afb4c21e8fde2
arthur albuquerque <arthuralbuquerque.hsh@gmail.com>
2024-12-31 19:24:29 -0300

    Ultima Att

--- Pages/Categories.cshtml.cs
+++ Pages/Categories.cshtml.cs
@@ -6,18 +6,24 @@ namespace MyApp.Namespace
     public class CategoriesModel : PageModel
     {
         public List<Category>Categories{ get; set; }= new ();
-        public void OnGet()
+        public void OnGet(
+            int skip = 0,
+            int take= 25)
         {
-            
+            var temp = new List<Category> ();
             //await Task.Delay(5000);//adiciona um delay de 5 segundos no carregamento da tela tentando buscar algo
-            for (var i = 0;i <= 100;i++)
+            for (var i = 0;i <= 500;i++)
             {
-                Categories.Add(
+                temp.Add(
                     new Category (
                     i,//id
                     $"Categoria{i}",//Title
                     i*18.95M));//Price
             }
+            Categories = temp
+            .Skip(skip)
+            .Take(take)
+            .ToList();
         }
     
     }
